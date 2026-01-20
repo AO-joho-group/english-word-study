a4b2d822cb5dde7b24aca28f1fc97e980f7f9103
JabaraNo8 <rrremon.1212@gmail.com>
2026-01-21 01:10:24 +0900

    Update input.js

--- input.js
+++ input.js
@@ -1,1 +1,10 @@
-alert("THIS IS INPUT.JS");
+alert("INPUT JS LOADED");
+
+window.addEventListener("DOMContentLoaded", () => {
+  alert("DOM READY");
+
+  const checkBtn = document.getElementById("check");
+  const answerInput = document.getElementById("answer");
+
+  console.log(checkBtn, answerInput);
+});
